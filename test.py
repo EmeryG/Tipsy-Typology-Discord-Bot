23cdcf44e1698ff1bfe568bb3be624cd583c7a25
Ervin <notervin@gmail.com>
2018-11-19 10:51:37 -0600

    Started working on adding data tracking

--- test.py
+++ test.py
@@ -20,14 +20,23 @@ async def on_message(message):
 async def on_member_update(before, after):
     if before.name != after.name:
         await updateName(before.name, after.name)
-		
+
+@client.event
+async def on_member_join(member):
+    try:
+        cur.execute("SELECT id FROM players WHERE id = " + member.id)
+    except:
+        cur.execute("INSERT INTO players ")
+    finally:
+        db.commit();
+
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
-	
+
 def updateName(before, after):
     try:
         cur.execute("UPDATE players SET name = " + after.name + " WHERE name = '" + before.name +"'")
@@ -47,11 +56,11 @@ def addPoints(name, number):
 
 def setTeam(name, team):
 	try:
-		cur.execute("UPDATE players SET team = " + team + " WHERE name = '" + before.name +"'")
+		cur.execute("UPDATE players SET team = " + team + " WHERE name = '" + name +"'")
 	except:
-		cur.execute("INSERT INTO players VALUES ('" + after.name + "',0,0,'" + team + "')")
+		cur.execute("INSERT INTO players VALUES ('" + name + "',0,0,'" + team + "')")
 	finally:
 		db.commit()
-    
+
 client.run('NTA2NTk4MjY0NjY2MDYyODY4.Drk4cw.7Gy8m9e_T5ncDY370aitXq085OE')
 
